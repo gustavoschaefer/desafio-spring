e5d5b15ab36deb2996d4845bc1e76fd7a38b84ff
jefamorim <jeferson.amorim@mercadolivre.com>
2021-12-29 11:24:46 -0300

    Cadastrar lista de produtos

---
+++ src/main/java/com/meli/w4/desafiospring/repository/ProdutoRepository.java
@@ -0,0 +1,35 @@
+package com.meli.w4.desafiospring.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.meli.w4.desafiospring.entity.Produto;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class ProdutoRepository implements ProdutoInterface<Produto,Map<String,String>>{
+
+    private List<Produto> produtos = new ArrayList<>();
+    private ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    private final String  PATH = "src/main/java/com/meli/w4/desafiospring/repository/produtos.json";
+
+
+    @Override
+    public void salvarProdutos(List<Produto> listaProdutos) throws IOException {
+
+        produtos.addAll(listaProdutos);
+
+        objectMapper.writeValue(new File(PATH),produtos);
+    }
+
+    @Override
+    public List<Produto> getProdutos(Map<String, String> params) throws IOException {
+        return null;
+    }
+}
--- src/main/java/com/meli/w4/desafiospring/service/ProdutoService.java
+++ src/main/java/com/meli/w4/desafiospring/service/ProdutoService.java
@@ -1,8 +1,31 @@
 package com.meli.w4.desafiospring.service;
 
+import com.meli.w4.desafiospring.entity.Produto;
+import com.meli.w4.desafiospring.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProdutoService {
-    
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public void setProdutos(List<Produto> produtos) {
+        try {
+            produtoRepository.salvarProdutos(produtos);
+        } catch (IOException e){
+            System.out.println("deu ruim");
+        }
+
+    }
+
+    public List<Produto> getProdutos(Map<String,String> param) throws IOException{
+
+        return null;
+    }
 }
